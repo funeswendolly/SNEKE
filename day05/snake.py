17db2e693b30ee24833cc53a6b38e281115d290b
Wendolly Funes Prado <funeswen001@warren.k12.in.us>
2025-11-24 08:03:21 -0500

    added starter variables, font and sounds

--- day05/snake.py
+++ day05/snake.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import pygame, random
 
 # Initialize pygame
@@ -39,7 +41,7 @@ title_rect.center = (WINDOW_WIDTH // 2,
 
 score_text = font.render("Score:0 " + str(score), True, GREEN, DARKRED)
 score_rect = score_text.get_rect()
-score_rect.center = (WINDOW_WIDTH // 10, WINDOW_HEIGHT // 10)
+score_rect.topleft = (10, 10)
 
 game_over_text = font.render("GAME OVER", True, RED, DARKRED)
 game_over_rect = game_over_text.get_rect()
@@ -59,7 +61,10 @@ apple_rect = pygame.draw.rect(display_surface, RED, apple_coord)
 head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
 head_rect = pygame.draw.rect(display_surface, GREEN, head_coord)
 
+body_coords = []
+
 running = True
+is_paused = False
 while running:
     # Check to see if the user wants to quit
     for event in pygame.event.get():
@@ -74,25 +79,25 @@ while running:
             if event.key == pygame.K_RIGHT:
                 snake_dx = SNAKE_SIZE
                 snake_dy = 0
-            if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
                 snake_dx = 0
                 snake_dy = -1 * SNAKE_SIZE
             if event.key == pygame.K_DOWN:
-                snake_dy = 0
-                snake_dx = SNAKE_SIZE
+                snake_dx = 0
+                snake_dy = SNAKE_SIZE
 
     # Add the head coordinate to the first index of the body coordinate list
     # This will essentially move all the snakes body by one position in the list
     body_coords.insert(0, head_coord)
-    body_coords.pop(0)
+    body_coords.pop()
     # Update the x,y position of the snakes head and make a new coordinate
-    head_x = snake_dx
-    head_y = snake_dy
+    head_x += snake_dx
+    head_y += snake_dy
     head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
     # Check for game over
     if head_rect.left < 0 or head_rect.right > WINDOW_WIDTH or head_rect.top < 0 or head_rect.bottom > WINDOW_HEIGHT or head_coord in body_coords:
         display_surface.blit(game_over_text, game_over_rect)
-        display_surface.blit(continue_text,continue_rect)
+        display_surface.blit(continue_text, continue_rect)
         pygame.display.update()
 
         # Pause the game until the player presses a key, then reset the game
@@ -103,9 +108,10 @@ while running:
                 if event.type == pygame.QUIT:
                     is_paused = False
                     running = False
-                if  event.type == pygame.KEYDOWN:
+                if event.type == pygame.KEYDOWN:
                     score = 0
-                    head_y =WINDOW_HEIGHT // 2 +100
+                    head_x = WINDOW_WIDTH // 2
+                    head_y = WINDOW_HEIGHT // 2 + 100
                     head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
                     body_coords = []
                     snake_dx = 0
@@ -122,7 +128,7 @@ while running:
         body_coords.append(head_coord)
 
     # Update HUD
-    score_text = font.render(f"Score {score}" + str(score), True, GREEN, DARKRED)
+    score_text = font.render(f"Score {score}", True, GREEN, DARKRED)
 
     # Fill the surface
     display_surface.fill(WHITE)
