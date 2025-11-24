3bccbf83fdb6b29af6c3ba3f38ed55f503a98ec7
Wendolly Funes Prado <funeswen001@warren.k12.in.us>
2025-11-24 07:20:46 -0500

    added starter variables, font and sounds

--- day05/snake.py
+++ day05/snake.py
@@ -1,6 +1,4 @@
-import pygame
-
-from day04.snake_notes import body_coords
+import pygame, random
 
 # Initialize pygame
 pygame.init()
@@ -88,10 +86,40 @@ while running:
     body_coords.insert(0, head_coord)
     body_coords.pop(0)
     # Update the x,y position of the snakes head and make a new coordinate
-
+    head_x = snake_dx
+    head_y = snake_dy
+    head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
     # Check for game over
+    if head_rect.left < 0 or head_rect.right > WINDOW_WIDTH or head_rect.top < 0 or head_rect.bottom > WINDOW_HEIGHT or head_coord in body_coords:
+        display_surface.blit(game_over_text, game_over_rect)
+        display_surface.blit(continue_text,continue_rect)
+        pygame.display.update()
+
+        # Pause the game until the player presses a key, then reset the game
+        is_paused = True
+        while is_paused:
+            for event in pygame.event.get():
+                # The player wants to quit
+                if event.type == pygame.QUIT:
+                    is_paused = False
+                    running = False
+                if  event.type == pygame.KEYDOWN:
+                    score = 0
+                    head_y =WINDOW_HEIGHT // 2 +100
+                    head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
+                    body_coords = []
+                    snake_dx = 0
+                    snake_dy = 0
+                    is_paused = False
 
     # Check for collisions
+    if head_rect.colliderect(apple_rect):
+        score += 1
+        pick_up_sound.play()
+        apple_x = random.randint(0, WINDOW_WIDTH - SNAKE_SIZE)
+        apply_y = random.randint(0, WINDOW_HEIGHT - SNAKE_SIZE)
+        apple_coord = (apple_x, apply_y, SNAKE_SIZE, SNAKE_SIZE)
+        body_coords.append(head_coord)
 
     # Update HUD
     score_text = font.render(f"Score {score}" + str(score), True, GREEN, DARKRED)
